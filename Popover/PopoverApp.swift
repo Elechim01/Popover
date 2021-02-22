fd5f980979efe73b79479586c077bc9ff388c89c
Elechim01 <mikymanny015@gmail.com>
2021-02-22 16:50:04 +0100

    Initial Commit

---
+++ Popover/PopoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopoverApp.swift
+//  Popover
+//
+//  Created by Michele Manniello on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct PopoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
